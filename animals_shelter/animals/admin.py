9ee299e25a51e3b023c0cafd21875245b65ff8ac
Maksim Strelchenko <bender404123@gmail.com>
2023-02-03 14:44:53 +0300

    add actions for admin panel

--- animals_shelter/animals/admin.py
+++ animals_shelter/animals/admin.py
@@ -9,3 +9,12 @@ class AnimalAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'arrival_date', 'is_deleted']
     list_filter = ['arrival_date', 'is_deleted']
     search_fields = ['name', 'additional_information']
+    actions = ['mark_as_deleted', 'unmark_as_deleted']
+
+    @admin.action(description='Mark Animal as Deleted (Soft Delete)')
+    def mark_as_deleted(self, request, queryset):
+        queryset.update(is_deleted=True)
+
+    @admin.action(description='Unmark Animal as Deleted')
+    def unmark_as_deleted(self, request, queryset):
+        queryset.update(is_deleted=False)
