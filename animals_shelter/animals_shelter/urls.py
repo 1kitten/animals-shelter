cbcc6e701326b1aa368052909d1d85055b23c7f8
Maksim Strelchenko <bender404123@gmail.com>
2023-02-03 13:45:27 +0300

    add authentication

--- animals_shelter/animals_shelter/urls.py
+++ animals_shelter/animals_shelter/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/auth/', include('rest_framework.urls')),
     path('api/v1/animals/', include('animals.urls'))
 ]
