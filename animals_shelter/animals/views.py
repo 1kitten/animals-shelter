536793e6d11442021320ddca7cf9ac9d4add4934
Maksim Strelchenko <bender404123@gmail.com>
2023-02-03 12:54:08 +0300

    Add permission for API List View.

--- animals_shelter/animals/views.py
+++ animals_shelter/animals/views.py
@@ -1,8 +1,16 @@
 from rest_framework import generics
+from .permissions import IsAdminOrReadOnly
 from .serializers import AnimalSerializer
 from .models import Animal
 
 
-class AnimalsAPIList(generics.ListAPIView):
+class AnimalsAPIList(generics.ListCreateAPIView):
+    """
+    API List View.
+    Returns all the values in Animals table from DataBase.
+    Returned format is Json.
+    User Allowed to create new Animal on page.
+    """
     serializer_class = AnimalSerializer
     queryset = Animal.objects.all()
+    permission_classes = [IsAdminOrReadOnly]
