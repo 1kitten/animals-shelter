646dee4a053f0e0506bee49666a863348cdfc19c
Maksim Strelchenko <bender404123@gmail.com>
2023-02-03 13:37:48 +0300

    add comment for permissions

--- animals_shelter/animals/permissions.py
+++ animals_shelter/animals/permissions.py
@@ -2,6 +2,10 @@ from rest_framework import permissions
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
+    """
+    Permission to check out if user is Admin
+    if he is - return True. Else return False.
+    """
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
@@ -10,6 +14,10 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
 
 class IsAdminOrAuthenticatedOrReadOnly(permissions.BasePermission):
+    """
+    Permission to check out if user is Authenticated or Admin.
+    If he is - return True. Else return False
+    """
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
