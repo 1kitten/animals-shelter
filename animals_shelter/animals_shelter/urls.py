7d36a956da0d26cf1b3e2aee51e33ae0fff56b91
Maksim Strelchenko <bender404123@gmail.com>
2023-02-03 14:59:55 +0300

    add swagger documentation for API

--- animals_shelter/animals_shelter/urls.py
+++ animals_shelter/animals_shelter/urls.py
@@ -15,9 +15,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Animals in Shelter API",
+        default_version="v1",
+        description="API Gives information about all the animals in out shelters!",
+        terms_of_service="https://google.com/policies/terms/",
+        contact=openapi.Contact(email="maksim.strelchenko99@gmail.com"),
+        license=openapi.License(name="")
+    ),
+    public=True,
+    permission_classes=[permissions.IsAuthenticatedOrReadOnly],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth/', include('rest_framework.urls')),
-    path('api/v1/animals/', include('animals.urls'))
+    path('api/v1/animals/', include('animals.urls')),
+    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0))
 ]
